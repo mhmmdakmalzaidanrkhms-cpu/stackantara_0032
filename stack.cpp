0269da4750fc0ee2d3643530c71caf7732d3d029
mhmmdakmalzaidanrkhms <mhmmdakmalzaidanrkhms@gmail.com>
2026-08-13 10:07:28 +0700

    beri value next sebagai null

--- stack.cpp
+++ stack.cpp
@@ -7,4 +7,9 @@ public:
     // 2. inisialisasi variabel data dan Node next sebagai member class
     int data;
     Node* next;
+
+    Node() {
+        // 3. pemberian value next sebagai NULL
+        next = NULL;
+    }
 };
