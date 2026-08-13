0d80467bd0b9e112ec0c1287eff79353e5d6de30
mhmmdakmalzaidanrkhms <mhmmdakmalzaidanrkhms@gmail.com>
2026-08-13 10:13:43 +0700

    add metode push

--- stack.cpp
+++ stack.cpp
@@ -26,4 +26,15 @@ public:
         // 6. inisialisasi nilai awal top sebagai NULL
         top = NULL;
     }
+
+    // 7. pembuatan method push
+    int push(int value) {
+        Node* newNode = new Node();
+        newNode->data = value;
+        newNode->next = top;
+        top = newNode;
+
+        cout << "Push Value: " << value << endl;
+        return value;
+    }
 };
